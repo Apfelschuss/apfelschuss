1530e1f3ec28acba846a6c16c5428223fba861c4
philippbruhin <info@philippbruhin.ch>
2019-04-17 22:08:22 +0200

    updated voting tests

--- apfelschuss/votes/tests/test_urls.py
+++ apfelschuss/votes/tests/test_urls.py
@@ -5,4 +5,4 @@ class TestUrls:
 
     def test_detail_url(self):
         path = reverse('votes:single', kwargs={'id': 1})
-        assert resolve(path).view_name == 'votes:single'
+        assert resolve(path).view_name == 'votes:votes_single'
