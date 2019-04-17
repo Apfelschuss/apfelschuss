4c0c9d69081b1f3876153f5fe823228d01b995d2
philippbruhin <info@philippbruhin.ch>
2019-04-17 22:05:42 +0200

    updated voting model drastically

--- apfelschuss/votes/models.py
+++ apfelschuss/votes/models.py
@@ -9,6 +9,8 @@ User = get_user_model()
 
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     profile_picture = models.ImageField()
 
     def __str__(self):
@@ -17,6 +19,10 @@ class Author(models.Model):
 
 class Category(models.Model):
     title = models.CharField(max_length=80)
+    voting_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
@@ -24,14 +30,20 @@ class Category(models.Model):
 
 class Voting(models.Model):
     title = models.CharField(max_length=160)
-    overview = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-    content = HTMLField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=True)
+    description = HTMLField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     thumbnail = models.ImageField()
     video_url = models.URLField(max_length=200)
+    pro_url = models.URLField(max_length=200)
+    contra_url = models.URLField(max_length=200)
+    admin_url = models.URLField(max_length=200)
+    admin_brochure = models.FileField(upload_to='uploads/')
+    admin_pro = models.DecimalField(max_digits=4, decimal_places=1)
     categories = models.ManyToManyField(Category)
-    featured = models.BooleanField(default=True)
+    featured = models.BooleanField(default=False)
     previous_voting = models.ForeignKey(
         'self',
         related_name='previous',
