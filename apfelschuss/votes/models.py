826d871979003494f63c0188eaaecf1c84134f3a
philippbruhin <info@philippbruhin.ch>
2019-04-19 01:59:04 +0200

    updated voting model names

--- apfelschuss/votes/models.py
+++ apfelschuss/votes/models.py
@@ -2,58 +2,118 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
+from filebrowser.fields import FileBrowseField
 from tinymce import HTMLField
 
 User = get_user_model()
 
 
 class Author(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    profile_picture = models.ImageField()
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+        )
+    updated_at = models.DateTimeField(
+        auto_now=True
+        )
+    profile_picture = models.ImageField(
+        blank=True
+        )
 
     def __str__(self):
         return self.user.username
 
 
 class Category(models.Model):
-    title = models.CharField(max_length=80)
-    voting_date = models.DateTimeField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    published = models.BooleanField(default=True)
+    title = models.CharField(
+        max_length=80,
+        verbose_name = "Voting category title"
+        )
+    voting_date = models.DateTimeField(
+        verbose_name = "Voting final date"
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+        )
+    updated_at = models.DateTimeField(
+        auto_now=True
+        )
+    published = models.BooleanField(
+        default=True
+        )
 
     def __str__(self):
         return self.title
 
 
 class Voting(models.Model):
-    title = models.CharField(max_length=160)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    published = models.BooleanField(default=True)
-    description = HTMLField()
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    thumbnail = models.ImageField()
-    video_url = models.URLField(max_length=200)
-    pro_url = models.URLField(max_length=200)
-    contra_url = models.URLField(max_length=200)
-    admin_url = models.URLField(max_length=200)
-    admin_brochure = models.FileField(upload_to='uploads/')
-    admin_pro = models.DecimalField(max_digits=4, decimal_places=1)
-    categories = models.ManyToManyField(Category)
-    featured = models.BooleanField(default=False)
+    title = models.CharField(
+        max_length=160,
+        verbose_name = "Voting title"
+        )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+        )
+    updated_at = models.DateTimeField(
+        auto_now=True
+        )
+    published = models.BooleanField(
+        default=True,
+        verbose_name = "Voting published"
+        )
+    description = HTMLField(
+        verbose_name = "Voting description"
+        )
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        )
+    thumbnail = FileBrowseField(
+        "Thumbnail",
+        max_length=200,
+        directory="uploads/",
+        blank=True,
+        )
+    video_url = models.URLField(
+        max_length=200,
+        verbose_name = "Youtube embedded URL",
+        blank=True
+        )
+    admin_brochure = FileBrowseField(
+        "Formal brochure",
+        max_length=200,
+        directory="uploads/",
+        blank=True,
+        )
+    admin_pro = models.DecimalField(
+        max_digits=4,
+        decimal_places=1,
+        verbose_name = "Final result pro [%]",
+        blank=True,
+        )
+    categories = models.ManyToManyField(
+        Category,
+        verbose_name = "Voting category"
+        )
+    featured = models.BooleanField(
+        default=False,
+        verbose_name = "Show on home"
+        )
     previous_voting = models.ForeignKey(
         'self',
         related_name='previous',
         on_delete=models.SET_NULL,
-        blank=True, null=True)
+        blank=True, null=True
+        )
     next_voting = models.ForeignKey(
         'self',
         related_name='next',
         on_delete=models.SET_NULL,
-        blank=True, null=True)
+        blank=True, null=True
+        )
 
     def __str__(self):
         return self.title
