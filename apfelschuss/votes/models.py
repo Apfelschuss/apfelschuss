831027dcaee9e12aa2e1800b5062ddc71df5e43a
philippbruhin <info@philippbruhin.ch>
2019-05-13 23:45:21 +0200

    updated voting model

--- apfelschuss/votes/models.py
+++ apfelschuss/votes/models.py
@@ -16,13 +16,13 @@ class Author(models.Model):
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
-        )
+    )
     created_at = models.DateTimeField(
         auto_now_add=True
-        )
+    )
     updated_at = models.DateTimeField(
         auto_now=True
-        )
+    )
 
     def __str__(self):
         return self.user.username
@@ -32,33 +32,33 @@ class Category(models.Model):
     title = models.CharField(
         max_length=80,
         verbose_name="Voting category title"
-        )
+    )
     slug = models.SlugField(
         max_length=80,
         unique=True,
         verbose_name="Voting category URL slug",
         blank=True
-        )
+    )
     voting_date = models.DateTimeField(
         verbose_name="Voting final date"
-        )
+    )
     author = models.ForeignKey(
         Author,
         on_delete=models.CASCADE,
-        )
+    )
     created_at = models.DateTimeField(
         auto_now_add=True
-        )
+    )
     updated_at = models.DateTimeField(
         auto_now=True
-        )
+    )
     published = models.BooleanField(
         default=False
-        )
+    )
     featured = models.BooleanField(
         default=False,
         verbose_name="Featured category"
-        )
+    )
 
     def __str__(self):
         return self.title
@@ -76,7 +76,7 @@ class Voting(models.Model):
             "it": {"blank": True},
             "rm": {"blank": True},
             "en": {"blank": True},
-        },
+        }
     )
     slug = TranslatedField(
         models.SlugField(
@@ -89,18 +89,18 @@ class Voting(models.Model):
             "it": {"blank": True, "unique": True},
             "rm": {"blank": True, "unique": True},
             "en": {"blank": True, "unique": True},
-        },
+        }
     )
     created_at = models.DateTimeField(
         auto_now_add=True
-        )
+    )
     updated_at = models.DateTimeField(
         auto_now=True
-        )
+    )
     published = models.BooleanField(
         default=False,
         verbose_name="Voting published"
-        )
+    )
     description = TranslatedField(
         HTMLField(
             verbose_name="Voting description",
@@ -111,55 +111,59 @@ class Voting(models.Model):
             "it": {"blank": True},
             "rm": {"blank": True},
             "en": {"blank": True},
-        },
-        )
+        }
+    )
     author = models.ForeignKey(
         Author,
         on_delete=models.CASCADE,
-        )
+    )
     thumbnail = FileBrowseField(
         "Thumbnail",
         max_length=200,
         directory="uploads/",
         blank=True,
-        )
-    video_url = models.URLField(
-        max_length=200,
-        verbose_name="Youtube embedded URL",
-        blank=True
-        )
-    admin_brochure = FileBrowseField(
-        "Formal brochure",
-        max_length=200,
-        directory="uploads/",
-        blank=True,
-        )
+    )
+    video_url = TranslatedField(
+        models.URLField(
+            max_length=200,
+            verbose_name="Youtube embedded URL",
+        ),
+        {
+            "de": {"blank": True},
+            "fr": {"blank": True},
+            "it": {"blank": True},
+            "rm": {"blank": True},
+            "en": {"blank": True},
+        }
+    )
+    admin_brochure = TranslatedField(
+        FileBrowseField(
+            max_length=200,
+            directory="uploads/",
+            verbose_name="Formal brochure",
+        ),
+        {
+            "de": {"blank": True},
+            "fr": {"blank": True},
+            "it": {"blank": True},
+            "rm": {"blank": True},
+            "en": {"blank": True},
+        }
+    )
     admin_pro = models.DecimalField(
         max_digits=4,
         decimal_places=1,
         verbose_name="Final result pro [%]",
         blank=True, null=True
-        )
+    )
     categories = models.ManyToManyField(
         Category,
         verbose_name="Voting category",
-        )
+    )
     featured = models.BooleanField(
         default=False,
         verbose_name="Featured voting"
-        )
-    previous_voting = models.ForeignKey(
-        'self',
-        related_name='previous',
-        on_delete=models.SET_NULL,
-        blank=True, null=True
-        )
-    next_voting = models.ForeignKey(
-        'self',
-        related_name='next',
-        on_delete=models.SET_NULL,
-        blank=True, null=True
-        )
+    )
 
     def __str__(self):
         return self.title
