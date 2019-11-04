e7796b4852ef83831ec432cd3b60745f5d23bf12
philippbruhin <info@philippbruhin.ch>
2019-11-04 21:29:30 +0100

    added polls to settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -75,6 +75,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     "apfelschuss.users.apps.UsersConfig",
     # Your stuff: custom apps go here
+    "apfelschuss.polls.apps.PollsConfig",   
     "apfelschuss.votes.apps.VotesConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
