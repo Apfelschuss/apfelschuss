d64b6987e90f477476e907a14e5b8f9589f409d6
philippbruhin <info@philippbruhin.ch>
2019-11-09 23:32:15 +0100

    added poll fields created_at and updated_at fields to admin panel

--- apfelschuss/polls/admin.py
+++ apfelschuss/polls/admin.py
@@ -4,10 +4,20 @@ from translated_fields import TranslatedFieldAdmin
 
 from .models import Author, Category, Poll
 
-admin.site.register(Author)
-admin.site.register(Category)
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['user', 'created_at', 'updated_at', 'id']
+    readonly_fields = ('created_at', 'updated_at', )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_at', 'updated_at', 'status', 'id']
+    readonly_fields = ('created_at', 'updated_at', )
 
 
 @admin.register(Poll)
 class PollAdmin(TranslatedFieldAdmin, admin.ModelAdmin):
-    pass
+    list_display = ['title', 'created_at', 'updated_at', 'featured', 'status', 'id']
+    readonly_fields = ('created_at', 'updated_at', )
