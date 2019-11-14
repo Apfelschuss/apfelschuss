0ed52f3c25dda7cf14b310518ae833cc51cf9100
philippbruhin <info@philippbruhin.ch>
2019-11-14 23:35:09 +0100

    added polls to seeder

--- apfelschuss/seeder.py
+++ apfelschuss/seeder.py
@@ -45,7 +45,7 @@ def seed_categories(num_entries=10, choice_min=2, choice_max=5, overwrite=False)
     Seeds num_entries category with random users as owners
     """
     if overwrite:
-        print('Overwriting category')
+        print('Overwriting categories')
         Category.objects.all().delete()
     users = list(User.objects.all())
     count = 0
@@ -54,13 +54,59 @@ def seed_categories(num_entries=10, choice_min=2, choice_max=5, overwrite=False)
             status = 'draft'
         else:
             status = 'published'  
-        c = Category( 
-            title = fake.sentence(),
+        c = Category(
             status = status,
+            title = fake.sentence(),
             poll_date = timezone.now(),
             owner = random.choice(users),
         )
         c.save()
+        count += 1
+        percent_complete = count / num_entries * 100
+        print(
+                "Adding {} new Categories: {:.2f}%".format(num_entries, percent_complete),
+                end='\r',
+                flush=True
+                )
+    print()
+
+
+def seed_polls(num_entries=10, choice_min=2, choice_max=5, overwrite=False):
+    """
+    Seeds num_entries polls with random categories and random users as owners
+    """
+    if overwrite:
+        print('Overwriting polls')
+        Poll.objects.all().delete()
+    users = list(User.objects.all())
+    category = list(Category.objects.all())
+    count = 0
+    for _ in range(num_entries):
+        if count % 6 == 0:
+            status = 'draft'
+        else:
+            status = 'published'  
+        p = Poll( 
+            status = status,
+            title_de = fake.sentence(),
+            title_fr = fake.sentence(),
+            title_it = fake.sentence(),
+            title_rm = fake.sentence(),
+            title_en = fake.sentence(),
+            description_de = fake.paragraph(),
+            description_fr = fake.paragraph(),
+            description_it = fake.paragraph(),
+            description_rm = fake.paragraph(),
+            description_en = fake.paragraph(),
+            video_url_de = 'https://www.youtube.com/embed/yltRgOFYD-w',
+            video_url_fr = 'https://www.youtube.com/embed/yltRgOFYD-w',
+            video_url_it = 'https://www.youtube.com/embed/yltRgOFYD-w',
+            video_url_rm = 'https://www.youtube.com/embed/yltRgOFYD-w',
+            video_url_en = 'https://www.youtube.com/embed/yltRgOFYD-w',
+            owner = random.choice(users),
+            category = random.choice(category),
+        )
+        p.save()
         count += 1
         percent_complete = count / num_entries * 100
         print(
@@ -80,7 +126,7 @@ def seed_all(num_entries=10, overwrite=False):
     # run seeds
     seed_users(num_entries=num_entries, overwrite=overwrite)
     seed_categories(num_entries=num_entries, overwrite=overwrite)
-    # seed_votes()
+    seed_polls(num_entries=num_entries, overwrite=overwrite)
     # get time
     elapsed_time = time.time() - start_time
     minutes = int(elapsed_time // 60)
